c4fcdbeea340640caf5c63f070330ebd89e5a097
RaviCampos <rnscampos@gmail.com>
2021-10-06 10:54:45 -0300

    commented instructions to run the server

--- api/handler.py
+++ api/handler.py
@@ -3,6 +3,14 @@ import pandas as pd
 from flask import Flask, request, Response
 from rossmann.Rossmann import Rossmann
 
+# ===========================
+# === TO START THE SERVER ===
+# run python handler.py
+# make a request to the open port via terminal executed file
+# or just a exexuted cell in jupyter notebook
+# there is already a test cell in this project jupyter notebook
+# ===========================
+
 # loading model
 model = pickle.load( open( "/home/derfel/ht/ds/ds_in_production/model_rossmann.pkl", "rb" ) )
 
@@ -37,8 +45,6 @@ def rossmann_predict():
         
         # prediction
         df_response = pipeline.get_prediction( model, test_raw, df3 )
-
-        
         
         return df_response
         
